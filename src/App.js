90f87bde25fac5a65ecf1823bfded325e8d26eda
Ratnadip Mondhe <rdmondhe96@gmail.com>
2021-07-29 14:27:17 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import {useState} from "react";
 
 
 function App() {
-let [name] =useState("cdac");
-let [counter]=useState(100);
-let [list]=useState(["delhi","goa"]);
-let [std1]=useState({name:"Diamond"});//object literals
 
+let [counter,setCounter]=useState(100);
+
+let updateCounter=()=>{
+  // alert("something happens");
+  let newvalue=counter+1;
+  setCounter(newvalue);
+}
  return(
    <div><h1>Learning to  declare stateful variable</h1>
-   <h1>String::{name} </h1>
-   <h1>Number ::{counter} </h1>
-   <h1>List ::{list} </h1>
-   <h1>Object ::{std1.name} </h1>
-   <h1>objetc::{JSON.stringify(std1)} </h1>
+   <h1>Counter:: {counter}</h1>
+   <input type="button" value="Increment" onClick ={updateCounter}/>
    </div>
- )
+ );
  
 
 
