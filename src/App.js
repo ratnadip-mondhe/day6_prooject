412541bc752a13955e3de2c080e873bb175452ec
Ratnadip Mondhe <rdmondhe96@gmail.com>
2021-07-29 14:18:56 +0530

    statefull

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let [name] =useState("cdac");
+let [counter]=useState(100);
+let [list]=useState(["delhi","goa"]);
+let [std1]=useState({name:"Diamond"});//object literals
+
+ return(
+   <div><h1>Learning to  declare stateful variable</h1>
+   <h1>String::{name} </h1>
+   <h1>Number ::{counter} </h1>
+   <h1>List ::{list} </h1>
+   <h1>Object ::{std1.name} </h1>
+   <h1>objetc::{JSON.stringify(std1)} </h1>
+   </div>
+ )
+ 
+
+
 }
 
 export default App;
