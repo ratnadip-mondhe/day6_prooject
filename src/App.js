34cd3d7c67e8c4171cb1e1b9f100c93ec225eaf7
Ratnadip Mondhe <rdmondhe96@gmail.com>
2021-07-30 23:27:55 +0530

    stateful variable

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState} from "react";
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  // Stateless member
-  let counter =100;
-  let increment=()=>{
-    counter +=1;
-    console.log(counter);
-  };
-return (
-  <div>
-    <h1>
-      Stateless doesnot update ui
-    </h1>
-    {counter}
-    <input type="button" value="Increment" onClick={increment} />
-  </div>
+  let [name] = useState("cdac"); // string varialble
+  let [counter] = useState(100); // number variable
+  let [active] = useState(false); // boolean
+  let [list] = useState(["delhi", "mumbai"]); // Arrays or List
+  let [std1] = useState({ name: "rahul" }); // Object Liiterals
 
-);
- 
-
+  return (
+    <div>
+      <h1>Learning to Declare Stateful Variable</h1>
+      <h1>String :: {name}</h1>
+      <h1>Number :: {counter}</h1>
+      <h1>Boolean :: {active + ""}</h1>
+      <h1>List :: {list}</h1>
+      <h1>Object :: {JSON.stringify(std1)}</h1>
+      <h1>Objec :; {std1.name} </h1>
+    </div>
+  );
 }
-
 
 export default App;
