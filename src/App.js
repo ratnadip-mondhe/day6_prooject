535652df3b149e54fb7e5ed4eb2a1a2c818a576e
Ratnadip Mondhe <rdmondhe96@gmail.com>
2021-07-30 23:25:04 +0530

    Stateless canot change ui

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import {useState} from "react";
 
 
 function App() {
+  // Stateless member
+  let counter =100;
+  let increment=()=>{
+    counter +=1;
+    console.log(counter);
+  };
 return (
-<div>
-  <h1 className="bg-primary text-light p=3">Hello Buddy</h1>
-</div>
+  <div>
+    <h1>
+      Stateless doesnot update ui
+    </h1>
+    {counter}
+    <input type="button" value="Increment" onClick={increment} />
+  </div>
 
 );
  
