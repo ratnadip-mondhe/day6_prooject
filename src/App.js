1dd563236a544c00b0d7f355cd28cba875d2e5d0
Ratnadip Mondhe <rdmondhe96@gmail.com>
2021-07-31 01:02:16 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function App() {
@@ -14,24 +14,25 @@ export default function App() {
 function MyRegisterComponent() {
   const [list, setList] = useState([]);
 
+  // React.
+  // useEffect();
+  // useEffect(() => {}, []);
+  useEffect(() => {
+    console.log("I am special. like your constructor.");
+    makeAjaxBackendApiCall();
+  }, []);
+
   const makeAjaxBackendApiCall = async () => {
     const url = "https://jsonplaceholder.typicode.com/posts";
     const result = await axios.get(url);
 
     // console.log(result.data);
-    const newlist = [...result.data, ...list];
-    setList(newlist);
+    setList(result.data);
   };
 
   return (
     <div>
       <h1 className="bg-primary text-light p-4">Lets connect with backend</h1>
-      <input
-        type="button"
-        className="btn btn-primary w-100 mb-1"
-        value="MAKE AJAX/BACKEND CALL"
-        onClick={makeAjaxBackendApiCall}
-      />
 
       {list.map((item) => {
         return (
