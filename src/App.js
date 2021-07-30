cc933a1ec8ff3420ce2c775c9ba2f0b3932097d3
Ratnadip Mondhe <rdmondhe96@gmail.com>
2021-07-30 23:31:54 +0530

    statfulcontinue

--- src/App.js
+++ src/App.js
@@ -3,16 +3,37 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let [counter, setCounter] = useState(100);
-  let [name, setName] = useState("rahul");
-  let [active, setActive] = useState(true);
+  let [counter, setCounter] = useState(0);
   let [list, setList] = useState(["delhi", "mumbai"]);
+
+  let updateCounter = () => {
+    let newvalue = counter + 100;
+    setCounter(newvalue);
+  };
+
+  let addNewElement = () => {
+    // alert("something is happening....");
+    let newlist = ["I am new Post", ...list];
+    setList(newlist);
+  };
 
   return (
     <div>
       <h1>Stateful Continue</h1>
-      <h1>Counter :: {counter}</h1>
-      <h1>LIst :: {list}</h1>
+      <h1>
+        Counter :: 👍 {counter}
+        <input type="button" value="Increment" onClick={updateCounter} />
+      </h1>
+      <hr />
+
+      <input
+        type="button"
+        value="Add New Element in List"
+        onClick={addNewElement}
+      />
+      {list.map((item) => {
+        return <h1>{item}</h1>;
+      })}
     </div>
   );
 }
