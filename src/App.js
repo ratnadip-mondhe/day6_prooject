f6c9055a2e0d5c1ae29e5b66a1c2d1547736b4ed
Ratnadip Mondhe <rdmondhe96@gmail.com>
2021-07-30 23:30:02 +0530

    statful varibale with  method

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let [name] = useState("cdac"); // string varialble
-  let [counter] = useState(100); // number variable
-  let [active] = useState(false); // boolean
-  let [list] = useState(["delhi", "mumbai"]); // Arrays or List
-  let [std1] = useState({ name: "rahul" }); // Object Liiterals
+  let [counter, setCounter] = useState(100);
+  let [name, setName] = useState("rahul");
+  let [active, setActive] = useState(true);
+  let [list, setList] = useState(["delhi", "mumbai"]);
 
   return (
     <div>
-      <h1>Learning to Declare Stateful Variable</h1>
-      <h1>String :: {name}</h1>
-      <h1>Number :: {counter}</h1>
-      <h1>Boolean :: {active + ""}</h1>
-      <h1>List :: {list}</h1>
-      <h1>Object :: {JSON.stringify(std1)}</h1>
-      <h1>Objec :; {std1.name} </h1>
+      <h1>Stateful Continue</h1>
+      <h1>Counter :: {counter}</h1>
+      <h1>LIst :: {list}</h1>
     </div>
   );
 }
