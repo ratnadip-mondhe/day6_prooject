64c08db310d62ffc9bcd0b9f684a1721098dc36e
Ratnadip Mondhe <rdmondhe96@gmail.com>
2021-07-30 23:37:47 +0530

    b8

--- src/App.js
+++ src/App.js
@@ -2,26 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
-function App() {
-  return (
-    <div>
-      <h1 className="bg-dark text-light p-3">Working with CSS Again</h1>
-
-      <h5 style={{ color: "white", backgroundColor: "royalblue" }}>
-        INLINE CSS :: Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-        Error eligendi vel reprehenderit. Cumque quia officia, totam sunt
-        tempora optio iste mollitia necessitatibus vel eius eligendi veniam
-        distinctio recusandae et ea?
-      </h5>
-
-      <p className="beafutify">
-        CSS FILE. App.css Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Inventore accusamus accusantium illum quam enim reprehenderit
-        assumenda consequatur impedit, eaque voluptatem aperiam vitae? Pariatur
-        deleniti molestiae modi tenetur porro temporibus quisquam?
-      </p>
-    </div>
-  );
+/**
+ * We are creating TAG :: User Defined Tag :: Component
+ * this needs to return JSX. :: In PascalCase
+ * <App />
+ *
+ * Pre Define tag :: IN SMALL CASE
+ * <h1 />
+ * <p /> <a /> <table />
+ * <h1 />
+ **/
+export default function App() {
+  return <div>Hello World</div>;
 }
 
-export default App;
+// export default App;
