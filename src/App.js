37318c12049cd5970aa594b899d59859bf37a2cb
Ratnadip Mondhe <rdmondhe96@gmail.com>
2021-07-30 23:54:02 +0530

    react router continue

--- src/App.js
+++ src/App.js
@@ -3,16 +3,57 @@ import "./App.css";
 import { useState } from "react";
 import { HelloWorld } from "./pages/HelloWorld";
 import { HelloUniverse } from "./pages/HelloUniverse";
+import { BrowserRouter, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <div>
-      <HelloWorld />
-      <HelloWorld />
-      <HelloWorld />
+    <BrowserRouter>
+      <Route exact={true} path="/page1" component={Page1} />
+      <Route exact={true} path="/page2" component={Page2} />
+      <Route exact={true} path="/page3" component={Page3} />
+      <Route exact={true} path="/" component={Page1} />
+    </BrowserRouter>
+  );
+}
 
-      <HelloUniverse />
-      <HelloUniverse />
+function Page1() {
+  return (
+    <div>
+      <h1>Page1 Lonavala</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod similique
+        aut sapiente delectus, dignissimos nesciunt perferendis velit ad
+        repellendus facere consequuntur debitis! Quidem iste dolor veniam
+        accusamus, molestiae commodi modi!
+      </p>
+    </div>
+  );
+}
+
+function Page2() {
+  return (
+    <div>
+      <h1>Page2 Goa</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod similique
+        aut sapiente delectus, dignissimos nesciunt perferendis velit ad
+        repellendus facere consequuntur debitis! Quidem iste dolor veniam
+        accusamus, molestiae commodi modi!
+      </p>
+    </div>
+  );
+}
+
+function Page3() {
+  return (
+    <div>
+      <h1>Page3 Delhi</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod similique
+        aut sapiente delectus, dignissimos nesciunt perferendis velit ad
+        repellendus facere consequuntur debitis! Quidem iste dolor veniam
+        accusamus, molestiae commodi modi!
+      </p>
     </div>
   );
 }
